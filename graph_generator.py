eb12df935cf00bcbb8f48f108dded6c27648b09e
Daniel Bulger <dpbulger@googlemail.com>
2014-05-07 02:33:23 -0400

    used python locks now

--- graph_generator.py
+++ graph_generator.py
@@ -1,6 +1,7 @@
 import networkx as nx
 from nltk.tree import ParentedTree
 import matplotlib.pyplot as plt
+import thread
 def get_distance_from_root(node):
 	depth = 0
 	current = node.parent()
@@ -35,33 +36,27 @@ class GraphGenerator:
 	def __init__(self):
 		self.graph = nx.Graph() # this graph can be added to
 		self.finalized_graph = None # this graph can be used for word sense disambiguation
-		self.locked = False
-	def lock(self):
-		while(self.locked):
-			pass
-		self.locked = True
-	def unlock(self):
-		self.locked = False
+		self.lock = thread.allocate_lock()
 	def update_graph(self,syntax_tree):
 		ptree = ParentedTree.convert(syntax_tree)
 		for leaf in get_leaves(ptree):
 			word = leaf[0]
-			self.lock()
+			self.lock.acquire()
 			if not word in self.graph:
 				self.graph.add_node(word,num=0)
 			self.graph.node[word]["num"] += 1
-			self.unlock()
+			self.lock.release()
 		for leaf in get_leaves(ptree):
 			word = leaf[0]
 			for other_leaf in get_leaves(ptree):
 				other_word = other_leaf[0]
 				if word == other_word:
 					continue
-				self.lock()
+				self.lock.acquire()
 				if not (word,other_word) in self.graph.edges():
 					self.graph.add_edge(word,other_word,weight=0)
 				self.graph.edge[word][other_word]["weight"] += 1.0/get_distance(leaf,other_leaf)
-				self.unlock()
+				self.lock.release()
 	def draw_graph(self,filename):
 		avg=sum([d["weight"] for (u,v,d) in self.graph.edges(data=True) ])/float(self.graph.number_of_edges())
 		elarge=[(u,v) for (u,v,d) in self.graph.edges(data=True) if d['weight'] >avg]
