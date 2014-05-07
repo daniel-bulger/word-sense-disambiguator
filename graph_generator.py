be338e6b8e03b0fecbad2283ae7a21de2dffcaf2
Daniel Bulger <dpbulger@googlemail.com>
2014-05-07 01:29:44 -0400

    LOCK Acquired

--- graph_generator.py
+++ graph_generator.py
@@ -35,22 +35,33 @@ class GraphGenerator:
 	def __init__(self):
 		self.graph = nx.Graph() # this graph can be added to
 		self.finalized_graph = None # this graph can be used for word sense disambiguation
+		self.locked = False
+	def lock(self):
+		while(self.locked):
+			pass
+		self.locked = True
+	def unlock(self):
+		self.locked = False
 	def update_graph(self,syntax_tree):
 		ptree = ParentedTree.convert(syntax_tree)
 		for leaf in get_leaves(ptree):
 			word = leaf[0]
+			lock()
 			if not word in self.graph:
 				self.graph.add_node(word,num=0)
 			self.graph.node[word]["num"] += 1
+			unlock()
 		for leaf in get_leaves(ptree):
 			word = leaf[0]
 			for other_leaf in get_leaves(ptree):
 				other_word = other_leaf[0]
 				if word == other_word:
 					continue
+				lock()
 				if not (word,other_word) in self.graph.edges():
 					self.graph.add_edge(word,other_word,weight=0)
 				self.graph.edge[word][other_word]["weight"] += 1.0/get_distance(leaf,other_leaf)
+				unlock()
 	def draw_graph(self,filename):
 		avg=sum([d["weight"] for (u,v,d) in self.graph.edges(data=True) ])/float(self.graph.number_of_edges())
 		elarge=[(u,v) for (u,v,d) in self.graph.edges(data=True) if d['weight'] >avg]
