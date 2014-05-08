e49ef0f04b93e419c35d9f00ff6374cbb0c6a462
Daniel Bulger <dpbulger@googlemail.com>
2014-05-08 05:18:13 -0400

    Fixed graph loading and improved sense generator

--- disambiguator/graph.py
+++ disambiguator/graph.py
@@ -18,9 +18,10 @@ def get_distance(node1,node2):
 		depth2 = get_distance_from_root(node2)
 		if depth1 >= depth2:
 			node1 = node1.parent()
+			distance += 1
 		if depth1 <= depth2:
 			node2 = node2.parent()
-		distance += 1
+			distance += 1
 	return distance
 def get_leaves(ptree):
 	leaves = [] 
@@ -46,8 +47,13 @@ def merge_graphs(graphs):
 	return new_graph
 
 class Graph(nx.Graph):
-	def __init__(self, graph=None):
+	def __init__(self):
 		nx.Graph.__init__(self)
+		self.cached_relatedness = None
+		self.normalized = False
+	def invalidate_cache(self):
+		self.cached_relatedness = None
+		self.normalized = False
 	def update(self,syntax_tree):
 		ptree = ParentedTree.convert(syntax_tree)
 		for leaf in get_leaves(ptree):
@@ -64,6 +70,7 @@ class Graph(nx.Graph):
 				if not (word,other_word) in self.edges():
 					self.add_edge(word,other_word,weight=0)
 				self.edge[word][other_word]["weight"] += 1.0/get_distance(leaf,other_leaf)
+		self.invalidate_cache()
 	def draw(self,filename = None):
 		avg=sum([d["weight"] for (u,v,d) in self.edges(data=True) ])/float(self.number_of_edges())
 		elarge=[(u,v) for (u,v,d) in self.edges(data=True) if d['weight'] >avg]
@@ -75,42 +82,86 @@ class Graph(nx.Graph):
 			plt.savefig(filename)  # pyplot draw()
 		else:
 			plt.show()
+	def normalize(self):
+		if self.normalized:
+			return
+		for edge in self.edges(data=True):
+			edge[2]["normalized_weight"] = edge[2]["weight"]/float(self.node[edge[0]]["num"]+self.node[edge[1]]["num"])
+		self.normalized = True
+	def ensure_normalized(self):
+		if not self.normalized:
+			self.normalize()
+	def get_median_relatedness(self):
+		if self.cached_relatedness != None:
+			return self.cached_relatedness
+		self.ensure_normalized()
+		weights = []
+		for edge in self.edges(data=True):
+			weights.append(edge[2]["normalized_weight"])
+		median = 0
+		if len(weights) % 2 == 0:
+			median = (weights[len(weights)/2]+weights[len(weights)/2 + 1]) / 2.0
+		else:
+			median = weights[len(weights)/2]
+		self.cached_relatedness = median
+		return median
+
 	def _partition_helper(self,senses,central_word):
-		for sense1 in senses:
-			for sense2 in senses:
+		# put similar things together
+		# for current_sense in senses:
+		# 	for word in current_sense:
+		# 		for other_sense in senses:
+		# 			if current_sense == other_sense:
+		# 				continue
+		# 			similarity_to_current_sense = 0.0
+
+
+		# get the appropriate number of senses
+		max_closeness = 0
+		closeness_indices = (0,0)
+		for i,sense1 in enumerate(senses):
+			for j,sense2 in enumerate(senses):
+				if i == j:
+					continue
 				closeness_to_center = 0.0
 				for word in sense1+sense2:
-					closeness_to_center += self.edge[word][central_word]["weight"]
+					closeness_to_center += self.edge[word][central_word]["normalized_weight"]
 				closeness_to_center /= float(len(sense1+sense2))
 				closeness_to_each_other = 0.0
 				for word1 in sense1:
 					for word2 in sense2:
 						if word2 in self.edge[word1]:
-							closeness_to_each_other += self.edge[word1][word2]["weight"]
+							closeness_to_each_other += self.edge[word1][word2]["normalized_weight"]
 				closeness_to_each_other /= float(len(sense1)*len(sense2))
-				if closeness_to_center < closeness_to_each_other:
-					sense1.extend(sense2)
-					senses.remove(sense2)
-					return self._partition_helper(senses,central_word)
+				if closeness_to_each_other > max_closeness:
+					max_closeness = closeness_to_each_other
+					closeness_indices = (i,j)
+		print senses
+		if self.get_median_relatedness() < max_closeness:
+			new_sense = senses[closeness_indices[1]][:]
+			senses[closeness_indices[0]].extend(new_sense)
+			senses.pop(closeness_indices[1])
+			return self._partition_helper(senses,central_word)
 		return senses
 
-	def partition(self,central_word):
-		senses = [[node] for node in nx.Graph.neighbors(self,central_word)]
+	def get_senses(self,central_word):
+		self.ensure_normalized()
+		senses = [[node] for node in nx.Graph.neighbors(self,central_word) 
+			if self.edge[node][central_word]["normalized_weight"] > self.get_median_relatedness()]
 		return self._partition_helper(senses,central_word)
 
-	def save(self):
-		nx.write_gml(self,"similarity_graph.gml")
+	def save_to_file(self,filename):
+		nx.write_gml(self,filename)
 
-	def load(self):
-		self.__dict__.update(nx.read_gml('similarity_graph.gml').__dict__)
+	def load_from_file(self,filename):
+		self.__dict__.update(nx.read_gml(filename).__dict__)
 
 		new_graph = Graph()
 		for node in self.nodes(data=True):
-			new_graph.add_node(node[1]["label"], num=node[1]["num"])
+			new_graph.add_node(str(node[1]["label"]), num=node[1]["num"])
 
 		for edge in self.edges(data=True):
-			new_graph.add_edge(self.node[edge[0]]["label"],
-							   self.node[edge[1]]["label"],
+			new_graph.add_edge(str(self.node[edge[0]]["label"]),
+							   str(self.node[edge[1]]["label"]),
 							   weight=edge[2]["weight"])
-
 		self.__dict__.update(new_graph.__dict__)
--- disambiguator/tests.py
+++ disambiguator/tests.py
@@ -7,6 +7,11 @@ trees = []
 trees.append(parser.parse("The food was on the table where the child likes to eat"))
 trees.append(parser.parse("The money is on the table"))
 trees.append(parser.parse("Put the data in the table"))
+trees.append(parser.parse("Add more rows to the database table"))
+trees.append(parser.parse("Add more rows to the database table"))
+trees.append(parser.parse("Why is the table empty It should have data in it"))
+trees.append(parser.parse("Do not put your elbows on the table while you eat"))
+
 for tree in trees:
 	tree = ParentedTree.convert(tree)
 graphs = []
@@ -15,5 +20,8 @@ for tree in trees:
 	g.update(tree)
 	graphs.append(g)
 new_graph = merge_graphs(graphs)
-print new_graph.partition("table")
 new_graph.draw("new_graph")
+new_graph.save_to_file("new_graph.gml")
+new_graph.load_from_file("new_graph.gml")
+print new_graph.get_median_relatedness()
+print new_graph.get_senses("table")
